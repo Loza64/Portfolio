76fae3e0baf8ca510ee5440ae317976497b02839
Roberto Loza <robertoloxa88@gmail.com>
2025-02-18 22:06:18 -0600

    fix errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {
-        path:'/', 
-        component: HomeComponent
+        path:'contact', 
+        component: ContactComponent
     },
     {
-        path:'/skills', 
+        path:'skills', 
         component: SkillsComponent
     }
 ];
