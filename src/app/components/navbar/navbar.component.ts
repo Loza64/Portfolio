ceae81560a5b1f600f42e4e12448bb8bdc8cfd27
Roberto Loza <robertoloxa88@gmail.com>
2025-03-05 17:54:09 -0600

    navbar responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
-
+import { Router, RouterLink, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
-  imports: [MatIconModule, RouterLink],
+  imports: [MatIconModule, RouterLink, RouterModule, CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router:Router){}
 
+  activeRoute(path: string): boolean {
+    return this.router.url === path;
+  }
 }
