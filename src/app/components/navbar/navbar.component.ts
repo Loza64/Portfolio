5f69bb31d140f8269973b3a070b63e60c412835b
Roberto Loza <robertoloxa88@gmail.com>
2025-03-06 15:52:29 -0600

    finish header

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink, RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   imports: [MatIconModule, RouterLink, RouterModule, CommonModule],
@@ -9,8 +10,9 @@ import { Router, RouterLink, RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
+
 export class NavbarComponent {
-  constructor(private router:Router){}
+  constructor(private router: Router) { }
 
   activeRoute(path: string): boolean {
     return this.router.url === path;
