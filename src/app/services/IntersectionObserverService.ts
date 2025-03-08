43178b8ea76878a0fd414bf74ece4f2da83b1d21
Roberto Loza <robertoloxa88@gmail.com>
2025-03-07 21:02:04 -0600

    update rootMargin

--- src/app/services/IntersectionObserverService.ts
+++ src/app/services/IntersectionObserverService.ts
@@ -24,7 +24,7 @@ export class IntersectionObserverService {
             },
             {
                 root: null,
-                rootMargin: '40px',
+                rootMargin: '1px',
                 threshold: 1
             }
         )
